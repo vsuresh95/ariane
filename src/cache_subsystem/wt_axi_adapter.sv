3650c51c16dbb84af568a5a946784b303b69fbde
Joseph Zuckerman <jzuck@cs.columbia.edu>
2021-08-11 01:11:10 -0400

    hold invalidation in adapter if conflicting read response is pending

--- src/cache_subsystem/wt_axi_adapter.sv
+++ src/cache_subsystem/wt_axi_adapter.sv
@@ -98,12 +98,14 @@ module wt_axi_adapter #(
   logic icache_rd_full, icache_rd_empty;
   logic dcache_rd_full, dcache_rd_empty;
   logic dcache_wr_full, dcache_wr_empty;
+  logic [riscv::PLEN-1:0] dcache_rtrn_rd_addr;
 
   // snooping on ACE
   logic                 axi_ac_valid;
   logic                 axi_ac_ready;
   logic [63:0]          axi_ac_addr;
   axi_pkg::prot_t       axi_ac_prot;
+  logic                 inval_gnt;
 
   assign icache_data_ack_o  = icache_data_req_i & ~icache_data_full;
   assign dcache_data_ack_o  = dcache_data_req_i & ~dcache_data_full;
@@ -329,6 +331,24 @@ module wt_axi_adapter #(
     .data_i     ( dcache_data.tid         ),
     .push_i     ( arb_ack[1] & axi_rd_gnt ),
     .data_o     ( dcache_rtrn_rd_tid      ),
+    .pop_i      ( dcache_rd_pop           )
+  );
+
+  //keep track of read address to check incoming invalidations for conflict
+  fifo_v3 #(
+    .DATA_WIDTH ( riscv::PLEN   ),
+    .DEPTH      ( MetaFifoDepth )
+  ) i_rd_dcache_addr (
+    .clk_i      ( clk_i                   ),
+    .rst_ni     ( rst_ni                  ),
+    .flush_i    ( 1'b0                    ),
+    .testmode_i ( 1'b0                    ),
+    .full_o     (                         ),
+    .empty_o    (                         ),
+    .usage_o    (                         ),
+    .data_i     ( dcache_data.paddr       ),
+    .push_i     ( arb_ack[1] & axi_rd_gnt ),
+    .data_o     ( dcache_rtrn_rd_addr     ),
     .pop_i      ( dcache_rd_pop           )
   );
 
@@ -564,14 +584,15 @@ module wt_axi_adapter #(
   assign icache_inv_addr_o = axi_ac_addr;
   assign dcache_inv_addr_o = axi_ac_addr;
   assign axi_ac_ready      = (icache_inv_ack_i & icache_inv_req_o) | (dcache_inv_ack_i & dcache_inv_req_o);
+  assign inval_gnt = (axi_ac_addr[riscv::PLEN-1:0] != dcache_rtrn_rd_addr) | dcache_rd_empty;
 
   always_comb begin : p_axi_snoop
      icache_inv_req_o = 1'b0;
      dcache_inv_req_o = 1'b0;
      if (axi_ac_prot[2])
-       icache_inv_req_o = axi_ac_valid;
+       icache_inv_req_o = axi_ac_valid & inval_gnt;
      else
-       dcache_inv_req_o = axi_ac_valid;
+       dcache_inv_req_o = axi_ac_valid & inval_gnt;
   end
 
 ///////////////////////////////////////////////////////
